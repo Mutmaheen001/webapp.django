f6bd0cc1ea5ffa7bd48a20a702353f3e089b828f
Mutmaheen001 <adeyanjumutmaheen@gmail.com>
2024-08-24 23:25:42 +0100

    web app working fine

---
+++ simpleSite/views.py
@@ -0,0 +1,8 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
+
+
+
+# def homepage(request):
+#     return render(request, 'info.htm')
